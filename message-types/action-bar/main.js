843021aa7a1f3e20352238f3ee4bb0b4d0601026
niravmehta <niravmehta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-19 18:25:53 +0000

    Removing old file
    git-svn-id: https://plugins.svn.wordpress.org/icegram/tags/1.8.6@1094554 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- message-types/action-bar/main.js
+++ message-types/action-bar/main.js
@@ -11,7 +11,7 @@ Icegram_Message_Type_Action_Bar.prototype.constructor = Icegram_Message_Type_Act
 Icegram_Message_Type_Action_Bar.prototype.get_template_default = function () {
     return  '<div class="icegram action_bar_{{=id}}" >'+
                 '<div class="action_bar ig_container {{=theme}}" id="icegram_message_{{=id}}">'+
-                    '<div class="ig_content clear">'+
+                    '<div class="ig_content ig_clear">'+
                         '<div class="ig_arrow_block" id="action_bar_close_{{=id}}">'+
                             '<span class="ig_arrow"></span>'+
                         '</div>'+
@@ -33,6 +33,10 @@ Icegram_Message_Type_Action_Bar.prototype.post_render = function ( ) {
     if( this.data.theme == 'hello' ) {
         var message_button = this.el.find('.ig_button');
         this.el.find('.ig_data').append(message_button);
+        // Temp Fix for verticle alignment of element in Hello Theme
+        if(this.el.find('.ig_embed_form').get(0)){
+            this.el.find('.ig_data').addClass('ig_valign_form_fix');
+        }
     }
 };
 
@@ -96,6 +100,7 @@ Icegram_Message_Type_Action_Bar.prototype.show = function ( options, silent ) {
             this.el.animate({
                 marginTop: 0
             }, 300);
+            //action bar
             break;
     }
     silent !== true && this.track( 'shown' );
